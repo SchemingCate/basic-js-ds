15032331d338b8c81c52f32f5e91ad7e3e84e19b
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-12-25 19:19:20 +0300

    feat: implement queue class

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,23 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.queue = [];
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.queue[0];
+  }
+
+  enqueue(value) {
+    this.queue.push(new ListNode(value));
+    if (this.queue.length > 1) {
+      this.queue[this.queue.length - 2].next = this.queue[this.queue.length - 1];
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue.shift().value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
